141c04e3b1df7a1bd1cfaf2396a3617ed5d74ecd
Rolf Kaiser <1090569+rkaiser0324@users.noreply.github.com>
2025-01-26 13:43:14 -0800

    can use rebase on HEAD~1 to reword via the batch file calling the PS file with the argument

---
+++ reword.bat
@@ -0,0 +1,15 @@
+@REM @echo off
+@REM set "filepath=%1"
+@REM for /f "tokens=*" %%a in ('type "%filepath%"') do (
+@REM   set "line=%%a"
+@REM   set "line=!line:pick=reword!"
+@REM   echo !line!>>temp.txt
+@REM )
+@REM del %filepath%
+@REM type temp.txt>%filepath%
+
+@echo off
+@REM set "filename=%1"
+echo "filename=%1"
+powershell.exe -File "H:\\shared\\digipowers\\vscode-git-graph\\reword-fromc.ps1" -FilePath %1
+exit /b 0
